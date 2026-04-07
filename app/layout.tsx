38cf0965f32c998a0802c26e3d5ab6c390588bb3
lahitofc-hash <lahit.ofc@gmail.com>
2026-04-07 00:54:36 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,14 @@
+import "./globals.css";
 
+export const metadata = {
+  title: "Linktree Artista",
+  description: "Links Oficiais",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="pt-br">
+      <body className="antialiased">{children}</body>
+    </html>
+  );
+}
