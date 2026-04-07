11426abb4e0ccfad056f56fe0060f8df6def4956
lahitofc-hash <lahit.ofc@gmail.com>
2026-04-07 01:22:36 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,10 @@ import Papa from "papaparse";
 import * as Icons from "lucide-react";
 
 // 1. PUBLIQUE SUA PLANILHA COMO CSV E COLE O LINK ABAIXO:
-const SHEET_URL = "SUA_URL_AQUI_TERMINANDO_EM_OUTPUT_CSV";
+const SHEET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSg2WzGx7rXW7B0aVTVOmv4_0OJ_9T43Ovk_-Y61yOmUhyq_kl5NYDDKV6FtJkUpMknnbGYLbmKExF_/pub?gid=0&single=true&output=csv";
 
 interface LinkItem {
-  label: string;
+  label: string;b
   url: string;
   icon: string;
   highlight: string;
